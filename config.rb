25aeb49ab2a4d3ded88ee68a43a35660e6264b7e
Apoorva Iyer <apoorva.iyer@gmail.com>
2015-05-18 01:23:44 -0700

    Changed deploy method to github. Got rid of sync gem config.

--- config.rb
+++ config.rb
@@ -53,6 +53,12 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
+activate :deploy do |deploy|
+  deploy.method = :git
+  deploy.branch = 'master'
+  deploy.build_before = true
+end
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
@@ -69,21 +75,4 @@ configure :build do
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
-  activate :sync do |config|
-    # config.verbose = false  # turn on verbose logging (defaults to false)
-    # config.force = false  # force syncing of outdated_files (defaults to false)
-    # config.run_on_build = true # when within a framework which `builds` assets, whether to sync afterwards (defaults to true)
-    # config.sync_outdated_files = true # when an outdated file is found whether to replace it (defaults to true)
-    # config.delete_abandoned_files = true # when an abandoned file is found whether to remove it (defaults to true)
-    # config.upload_missing_files = true # when a missing file is found whether to upload it (defaults to true)
-    # config.target_pool_size = 8 # how many threads you would like to open for each target (defaults to the amount of CPU core's your machine has)
-    # config.max_sync_attempts = 3 # how many times a file should be retried if there was an error during sync (defaults to 3)
-  end
-
-  activate :deploy do |deploy|
-    deploy.method = :git
-    deploy.branch = 'master'
-    deploy.build_before = true
-  end
-
 end
